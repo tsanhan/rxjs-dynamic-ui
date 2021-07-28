abb19dceb7cb0136b2ea0663d3ce0fdcb3bade64
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-07-28 22:19:02 +0300

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,6 @@ import { ActionNames } from './enums';
 /**
  * Use `ConterStateKeys` for property names.
  * Explort the counterUI API by typing `counterUI.` somewhere. ;)
- *
- * Implement all features of the counter:
- * 1. Start, pause the counter. Then restart the counter with 0 (+)
- * 2. Start it again from paused number (++)
- * 3. If Set to button is clicked set counter value to input value while counting (+++)
- * 4. Reset to initial state if reset button is clicked (+)
- * 5. If count up button is clicked count up, if count down button is clicked count down  (+)
- * 6. Change interval if input tickSpeed input changes (++)
- * 7. Change count up if input countDiff changes (++)
- * 8. Take care of rendering execution and other performance optimisations as well as refactoring (+)
  */
 
 // ==================================================================
@@ -30,6 +20,7 @@ import { ActionNames } from './enums';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   @ViewChild('TimerDisplay', { static: true }) TimerDisplay: ElementRef;
   @ViewChild('BtnStart', { static: true }) BtnStart: ElementRef;
   @ViewChild('BtnPause', { static: true }) BtnPause: ElementRef;
